80125cdeacc5f4990f9f866cbed3f2c99369fbdf
Sergei Larin <sergeo1024@gmail.com>
2022-01-07 16:38:32 +0700

    add application

---
+++ application/config.py
@@ -0,0 +1,22 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+
+
+class StagingConfig(BaseConfig):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(BaseConfig):
+    DEVELOPMENT = True
+    DEBUG = True
