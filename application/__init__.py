ccc64a31fe590a52e63a03e2d03ac23a23f35d1c
Sergei Larin <sergeo1024@gmail.com>
2022-03-05 01:56:15 +0700

    Fix runing error

--- application/__init__.py
+++ application/__init__.py
@@ -9,10 +9,11 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
 
+manager = LoginManager(app)
+
+
 from application.models import *
 from application.ui.views import ui
-
-manager = LoginManager(app)
 
 app.register_blueprint(ui)
 
