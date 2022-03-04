fb349c92cfda04101e1651af20a065f3dbac1381
Sergei Larin <sergeo1024@gmail.com>
2022-03-05 02:12:38 +0700

    Fix pylint error

--- iac/jenkins_scripts/cut_file.py
+++ iac/jenkins_scripts/cut_file.py
@@ -1,14 +1,8 @@
 TOKEN=""
-file_handle = open("jenkins_scripts/sonar-token.txt",'r',encoding='UTF-8')
-try:
-    content = file_handle.readlines()
+with open("jenkins_scripts/sonar-token.txt",'r',encoding='UTF-8') as file_read, \
+     open('jenkins_scripts/sonar-token.txt','w',encoding='UTF-8') as file_open:
+
+    content = file_read.readlines()
     for i in range(46, 86, 1):
         TOKEN=TOKEN + str(content)[i]
-finally:
-    file_handle.close()
-
-file_open = open('jenkins_scripts/sonar-token.txt','w',encoding='UTF-8')
-try:
     file_open.write(TOKEN)
-finally:
-    file_open.close()
