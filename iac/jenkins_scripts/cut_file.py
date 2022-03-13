96c7639a4d05cd6e049d8d802e6d9481bcc252bc
Sergei Larin <sergeo1024@gmail.com>
2022-03-14 03:44:21 +0700

    Fix Error for generate sonar token

--- iac/jenkins_scripts/cut_file.py
+++ iac/jenkins_scripts/cut_file.py
@@ -1,8 +1,11 @@
+file_path = 'jenkins_scripts/sonar-token.txt'
 TOKEN=""
-with open("jenkins_scripts/sonar-token.txt",'r',encoding='UTF-8') as file_read, \
-     open('jenkins_scripts/sonar-token.txt','w',encoding='UTF-8') as file_open:
+with open(file_path,'r',encoding='UTF-8') as file_read:
 
     content = file_read.readlines()
     for i in range(46, 86, 1):
         TOKEN=TOKEN + str(content)[i]
+
+with open(file_path,'w',encoding='UTF-8') as file_open:
     file_open.write(TOKEN)
+    file_open.close()
