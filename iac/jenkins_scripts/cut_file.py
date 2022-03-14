b81cccf92cbb1c9a467f8ccdc9811bda096a45f8
Sergei Larin <sergeo1024@gmail.com>
2022-03-15 00:10:03 +0700

    Fix. pylint fixed

--- iac/jenkins_scripts/cut_file.py
+++ iac/jenkins_scripts/cut_file.py
@@ -1,11 +1,11 @@
-file_path = 'jenkins_scripts/sonar-token.txt'
+FILE_PATH = 'jenkins_scripts/sonar-token.txt'
 TOKEN=""
-with open(file_path,'r',encoding='UTF-8') as file_read:
+with open(FILE_PATH,'r',encoding='UTF-8') as file_read:
 
     content = file_read.readlines()
     for i in range(46, 86, 1):
         TOKEN=TOKEN + str(content)[i]
 
-with open(file_path,'w',encoding='UTF-8') as file_open:
+with open(FILE_PATH,'w',encoding='UTF-8') as file_open:
     file_open.write(TOKEN)
     file_open.close()
